fad0de29796903b5efd89ee2f66078573bdf4c53
0x-Mr.7i74N <42717219+shivansh2310@users.noreply.github.com>
2023-03-06 11:35:14 +0530

    last push

---
+++ Polymorphism.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+using namespace std;
+
+class AbstractEmployee
+{
+    virtual void AskForPromotion() = 0;
+};
+
+class Employee : AbstractEmployee
+{
+private:
+    string Name;
+    string Company;
+    int Age;
+
+public:
+    void setName(string name) // Setter
+    {
+        Name = name;
+    }
+    string getName() // getter
+    {
+        return Name;
+    }
+    void setCompany(string company) //
+    {
+        Company = company;
+    }
+    string getCompany() //
+    {
+        return Company;
+    }
+    void setAge(int age) //
+    {
+        if (age >= 18)
+        {
+            Age = age;
+        }
+    }
+    int getAge() //
+    {
+        return Age;
+    }
+    void Intro()
+    {
+        cout << "Name: " << Name << endl;
+        cout << "Company: " << Company << endl;
+        cout << "age: " << Age << endl;
+    }
+    Employee(string name, string company, int age)
+
+    {
+        Name = name;
+        Company = company;
+        Age = age;
+    }
+    void AskForPromotion()
+    {
+        if (Age > 30)
+        {
+            cout << Name << ", is promoted" << endl;
+        }
+        else
+        {
+            cout << Name << ", is not promoted" << endl;
+        }
+    }
+    virtual void Work()
+    {
+        cout << Name << " is checking email " << endl;
+    }
+};
+
+// Inheritence Code
+
+class Developer : public Employee
+{
+public:
+    string FavProgrammingLang;
+    Developer(string name, string company, int age, string favProgrammingLang)
+        : Employee(name, company, age)
+    {
+        FavProgrammingLang = favProgrammingLang;
+    }
+    void FixBugs()
+    {
+        cout << getName() << ",is Fixing bugs using " << FavProgrammingLang << endl;
+    }
+
+    void Work()
+    {
+        cout << getName() << " is Writing Code  " << endl;
+    }
+};
+
+class Teacher : public Employee
+{
+public:
+    string Subject;
+    void PrepareLesson()
+    {
+        cout << getName() << " is preparing " << Subject << endl;
+    }
+    Teacher(string name, string company, int age, string subject)
+        : Employee(name, company, age)
+    {
+        Subject = subject;
+    }
+    void Work()
+    {
+        cout << getName() << " is Teaching Code  " << endl;
+    }
+};
+
+int main()
+{
+
+    Developer d = Developer("John", "Apple", 35, "Python");
+    Teacher t = Teacher("Jack", "KVK", 65, "Maths");
+
+    Employee *e1 = &d;
+    Employee *e2 = &t;
+
+    e1->Work();
+    e2->Work();
+
+    return 0;
+}
